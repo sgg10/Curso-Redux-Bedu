684e127ada9468b76b3b39515109a3fe4501b9ba
Sebastian Granda Gallego <sebastian.granda@jikkosoft.com>
2021-03-09 10:44:38 -0500

    Types File

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { TRAER_TODOS } from "../types/usuariosTypes";
 export const traerTodos = () => async (dispatch) => {
 	const payload = (await axios.get("https://jsonplaceholder.typicode.com/users")).data;
 	dispatch({
-		type: "traer_usuarios",
+		type: TRAER_TODOS,
 		payload,
 	});
 };

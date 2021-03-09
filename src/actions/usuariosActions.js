d4c2e19b968b485bf49028ec191934afcd1b0ee6
Sebastian Granda Gallego <sebastian.granda@jikkosoft.com>
2021-03-09 10:56:28 -0500

    Escenarios asíncronos

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,9 +1,16 @@
 import axios from "axios";
-import { TRAER_TODOS } from "../types/usuariosTypes";
+import { TRAER_TODOS, CARGANDO, ERROR } from "../types/usuariosTypes";
+
 export const traerTodos = () => async (dispatch) => {
-	const payload = (await axios.get("https://jsonplaceholder.typicode.com/users")).data;
-	dispatch({
-		type: TRAER_TODOS,
-		payload,
-	});
+	dispatch({ type: CARGANDO });
+	try {
+		const payload = (await axios.get("https://jsonplaceholder.typicode.com/users")).data;
+		dispatch({
+			type: TRAER_TODOS,
+			payload,
+		});
+	} catch (error) {
+		console.log("❗❗Error -> ", error.message);
+		dispatch({ type: ERROR, payload: error.message });
+	}
 };

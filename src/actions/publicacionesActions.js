728abc77464d949bb69577abd360d6bb2827612f
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-03-09 22:40:55 -0500

    Inmutabilidad

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { TRAER_POR_USUARIOS, CARGANDO, ERROR } from "../types/publicacionesTypes";
+import * as usuariosTypes from "../types/usuariosTypes";
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
 	dispatch({ type: CARGANDO });
@@ -10,6 +13,18 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
 	try {
 		const data = (await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuarios_id}`)).data;
 		const publicaciones_actualizadas = [...publicaciones, data];
+
+		const publicaciones_key = publicaciones_actualizadas.length - 1;
+		const usuarios_actualizados = [...usuarios];
+		usuarios_actualizados[key] = {
+			...usuarios[key],
+			publicaciones_key,
+		};
+
+		dispatch({
+			type: USUARIOS_TRAER_TODOS,
+			payload: usuarios_actualizados,
+		});
 		dispatch({
 			type: TRAER_POR_USUARIOS,
 			payload: publicaciones_actualizadas,

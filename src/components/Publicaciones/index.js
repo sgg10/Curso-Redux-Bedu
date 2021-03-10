a7d7f52ede44d6134a56fb89eca3b8cdf55c803d
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-03-09 22:55:56 -0500

    Evitar sobrescritura

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Spinner from "../common/Spinner";
+import Fatal from "../common/Fatal";
 
 import * as usuariosActions from "../../actions/usuariosActions";
 import * as publicacionesActions from "../../actions/publicacionesActions";
@@ -9,20 +11,44 @@ const { traerPorUsuario: publicacionesTraerPorUsuarios } = publicacionesActions;
 
 class Publicaciones extends Component {
 	async componentDidMount() {
+		const {
+			usuariosTraerTodos,
+			publicacionesTraerPorUsuarios,
+			match: {
+				params: { key },
+			},
+		} = this.props;
 		if (!this.props.usuariosReducers.usuarios.length) {
-			await this.props.usuariosTraerTodos();
+			await usuariosTraerTodos();
 		}
-		this.props.publicacionesTraerPorUsuarios(this.props.match.params.key);
+		if (this.props.usuariosReducers.error) {
+			return;
+		}
+		if (!("publicaciones_key" in this.props.usuariosReducers.usuarios[key])) {
+			publicacionesTraerPorUsuarios(key);
+		}
 	}
+
+	ponerUsuario = () => {
+		const {
+			usuariosReducers,
+			match: {
+				params: { key },
+			},
+		} = this.props;
+		if (usuariosReducers.error) {
+			return <Fatal message={usuariosReducers.error} />;
+		}
+		if (!usuariosReducers.usuarios.length || usuariosReducers.cargando) {
+			return <Spinner />;
+		}
+		const nombre = usuariosReducers.usuarios[key].name;
+		return <h1>Publicaciones de {nombre} </h1>;
+	};
 
 	render() {
 		console.log(this.props);
-		return (
-			<div>
-				<h1>Publicaciones de </h1>
-				{this.props.match.params.key}
-			</div>
-		);
+		return <div>{this.ponerUsuario()}</div>;
 	}
 }
 

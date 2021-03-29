bac295b9905d8b911c24eb094823a5793fe63d06
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-03-28 23:34:16 -0500

    Mapeando las tareas

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Spinner from "../common/Spinner";
+import Fatal from "../common/Fatal";
 
 import * as tareasActions from "../../actions/tareasActions";
 
@@ -8,13 +10,37 @@ class Tareas extends Component {
 		this.props.traerTodas();
 	}
 
+	mostrarContenido = () => {
+		const { tareas, cargando, error } = this.props.tareasReducers;
+		console.log(tareas, cargando, error);
+		if (cargando) {
+			return <Spinner />;
+		}
+		if (error) {
+			return <Fatal message={error} />;
+		}
+		return Object.keys(tareas).map((user_id) => (
+			<div key={user_id}>
+				<h2>Usuario {user_id}</h2>
+				<div className='contenedor_tareas'>{this.ponerTareas(user_id)}</div>
+			</div>
+		));
+	};
+
+	ponerTareas = (user_id) => {
+		const { tareas } = this.props.tareasReducers;
+		const por_usuario = { ...tareas[user_id] };
+		return Object.keys(por_usuario).map((tarea_id) => (
+			<div key={tarea_id}>
+				<input type='checkbox' defaultChecked={por_usuario[tarea_id].completed} />
+				{por_usuario[tarea_id].title}
+			</div>
+		));
+	};
+
 	render() {
 		console.log(this.props);
-		return (
-			<div>
-				<h1>Tareas Saludar</h1>
-			</div>
-		);
+		return <div>{this.mostrarContenido()}</div>;
 	}
 }
 

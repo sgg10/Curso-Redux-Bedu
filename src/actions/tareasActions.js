a9a4e5cac1b4f362b142e8cb5634ce8a3053082c
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-03-28 22:07:18 -0500

    Nuevo ciclo redux

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -5,9 +5,21 @@ export const traerTodas = () => async (dispatch) => {
 	dispatch({ type: CARGANDO });
 	try {
 		const payload = (await axios.get("https://jsonplaceholder.typicode.com/todos")).data;
+
+		const tareas = {};
+		payload.map(
+			(tarea) =>
+				(tareas[tarea.userId] = {
+					...tareas[tarea.userId],
+					[tarea.id]: {
+						...tarea,
+					},
+				})
+		);
+
 		dispatch({
 			type: TRAER_TODAS,
-			payload,
+			payload: tareas,
 		});
 	} catch (error) {
 		dispatch({ type: ERROR, payload: `❗❗Error: ${error.message}` });
